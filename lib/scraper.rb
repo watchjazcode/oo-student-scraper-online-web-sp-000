f9dc8feab59b82105c3668d96f5bdcabd9e1fb41
Jazmin M <jazminmerckel@gmail.com>
2019-11-23 19:25:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,42 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    students = []
+    doc.css(".student-card").each do |row|
+      students << {
+      :name => row.css(".student-name").text,
+      :location => row.css(".student-location").text,
+      :profile_url => row.css("a").attribute("href").value
+      }
+    end
+    return students
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    profile = {}
+    doc.css(".social-icon-container a").each do |link| 
+      href = link.attribute("href").value 
+    end
+    doc.css(".student-card")
+      {
+      :twitter => doc.css("a").attribute("href").value,
+      :linkedin => doc.css("a").attribute("href").value,
+      :github => doc.css("a").attribute("href").value,
+      :blog => doc.css("a").attribute("href").value,
+      :profile_quote => doc.css("a").attribute("href").value,
+      :bio => doc.css("a").attribute("href").value
+      }
   end
 
 end
+
+def scrape_horror_movies
+				rows = @doc.css("table.table tr")
+				rows.shift
+				rows[0..24].each do |row| 
+					listing = Horror::Movie::Listing.new(row)
+				end
+			end
 
